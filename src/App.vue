e8709011024af556fa734752d7d7d11214454240
Emmanuel <akhigbemmanuel@gmail.com>
2021-02-18 18:58:55 +0100

    Develop storefront page

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <vitty-header />
+    <vitty-hero class="mb-16" />
+    <vitty-home-products class="mb-16" title="New Products" />
+    <vitty-banner />
+  </div>
+</template>
+
+<script>
+  import VittyHero from "./components/VittyHero.vue";
+  import VittyHeader from "./components/VittyHeader.vue";
+  import VittyHomeProducts from "./components/VittyHomeProducts.vue";
+  import VittyBanner from "./components/VittyBanner.vue";
+  export default {
+    components: { VittyHero, VittyHeader, VittyHomeProducts, VittyBanner },
+  };
+</script>
+
+<style></style>
