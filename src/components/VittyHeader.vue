3256a2728e28896d9245c6d94e8b05eaa81ce8b0
Emmanuel <akhigbemmanuel@gmail.com>
2021-02-18 18:57:29 +0100

    Develop store header

---
+++ src/components/VittyHeader.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <SfHeader
+      logo="https://www.svgrepo.com/show/9395/ribbon-banner-silhouette.svg"
+      title="Storefront UI"
+      cartIcon="empty_cart"
+      wishlistIcon="heart"
+      accountIcon="profile"
+      searchPlaceholder="Search for items"
+      searchValue="Impossible"
+      wishlistItemsQty="0"
+      cartItemsQty="0"
+    >
+      <template v-slot:navigation>
+        <navigation-links />
+      </template>
+    </SfHeader>
+  </div>
+</template>
+
+<script>
+  import { SfHeader } from "@storefront-ui/vue";
+  import NavigationLinks from "./NavigationLinks.vue";
+  export default {
+    components: {
+      SfHeader,
+      NavigationLinks,
+    },
+  };
+</script>
+
+<style></style>
