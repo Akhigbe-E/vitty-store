6ac7f6c07550b7b1c3cb00c3d0d46e9d2fce742b
Emmanuel <akhigbemmanuel@gmail.com>
2021-02-18 18:57:47 +0100

    Develop navigation links

---
+++ src/components/NavigationLinks.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul class="flex gap-x-5">
+    <li><SfLink class="text-sf-lg" link="/">Home</SfLink></li>
+    <li><SfLink class="text-sf-lg" link="/about">About</SfLink></li>
+    <li><SfLink class="text-sf-lg" link="/contact">Contact</SfLink></li>
+  </ul>
+</template>
+
+<script>
+  import { SfLink } from "@storefront-ui/vue";
+  export default {
+    components: {
+      SfLink,
+    },
+  };
+</script>
+
+<style></style>
